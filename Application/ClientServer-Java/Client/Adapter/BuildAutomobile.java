d69e925579a3cec7a74eca7202cbf76c8765e6f5
Truc Huynh <jackyhuynh87@gmail.com>
2020-11-11 13:03:56 -0500

    Add files via upload

---
+++ Application/ClientServer-Java/Client/Adapter/BuildAutomobile.java
@@ -0,0 +1,9 @@
+package Adapter;
+
+import scale.Edit_Option;
+
+public class BuildAutomobile extends proxyAutomobile implements CreateAuto, 
+                                                    UpdateAuto, Edit_Option {	
+
+
+}
---
+++ Application/ClientServer-Java/Client/Adapter/UpdateAuto.java
@@ -0,0 +1,6 @@
+package Adapter;
+
+public interface UpdateAuto {
+	public void updateOptionSetName(String Model_Name,String Option_Set_Name, String Option_Set_New_Name);
+	public void updateOptionPrice (String Model_Name, String Option_Set_Name ,String Option_Name,float New_Price);
+}

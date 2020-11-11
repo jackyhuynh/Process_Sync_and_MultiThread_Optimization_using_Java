e02b570518b703a1b708bfa3b2bd50c425d3f6f0
Truc Huynh <jackyhuynh87@gmail.com>
2020-11-11 12:56:12 -0500

    Create Readme.java

---
+++ Application/ClientServer-Java/Readme.java
@@ -0,0 +1,9 @@
+# Author: Truc Huynh
+# Date: August 2013
+#
+# Description:
+# This is a basic client server application.
+# Not using any framework.
+# Application was buit from stratch
+# Server work with Apache Tomcat
+# For Reference only

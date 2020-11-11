0a3fdefd88a6d430661fce22a1a24b05e70fda51
Truc Huynh <jackyhuynh87@gmail.com>
2020-11-11 14:50:33 -0500

    Add files via upload

---
+++ Application/WebAPI-C#/Models/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace BackEnd2.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public string Secret { get; set; }
+    }
+
+
+}

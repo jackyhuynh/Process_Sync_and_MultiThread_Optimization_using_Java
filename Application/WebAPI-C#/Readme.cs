2f7e69db738f500294cd5db26e642b70b249d597
Truc Huynh <jackyhuynh87@gmail.com>
2020-11-11 14:46:16 -0500

    Create Readme.cs

---
+++ Application/WebAPI-C#/Readme.cs
@@ -0,0 +1,5 @@
+# Create a web API project.
+# Add a model class and a database context.
+# Scaffold a controller with CRUD methods.
+# Configure routing, URL paths, and return values.
+# Tutorial is retrieved from: https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-3.1&tabs=visual-studio

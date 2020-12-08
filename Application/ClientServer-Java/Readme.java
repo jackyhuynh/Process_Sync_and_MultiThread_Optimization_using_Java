b2f7e5c4cf63de4635955bf552167e042b70784e
Truc Huynh <jackyhuynh87@gmail.com>
2020-12-08 17:07:30 -0500

    New Ranch

--- Application/ClientServer-Java/Readme.java
+++
@@ -1,9 +0,0 @@
-# Author: Truc Huynh
-# Date: August 2013
-#
-# Description:
-# This is a basic client server application.
-# Not using any framework.
-# Application was buit from stratch
-# Server work with Apache Tomcat
-# For Reference only
